2fe223a5f47a581c2ed64d1480339865822f571d
Stefan Georgiev <stefan.georgiev91@gmail.com>
2021-10-28 19:39:11 +0300

    Update QuadraticEquation.java

--- src/exercise2/QuadraticEquation.java
+++ src/exercise2/QuadraticEquation.java
@@ -7,9 +7,9 @@ public class QuadraticEquation {
 public static void main(String args[]){
    Scanner input = new Scanner(System.in);
   
-  int a = input.nextInt();
-  int b = input.nextInt();
-  int c = input.nextInt();
+  double a = input.nextInt();
+  double b = input.nextInt();
+  double c = input.nextInt();
 
   double x;
   double x1;
@@ -21,8 +21,7 @@ public static void main(String args[]){
           x1 = (-b + Math.sqrt(D))/ 2*a;
           x2 = (-b - Math.sqrt(D))/ 2*a;
 
-          System.out.println(x1);
-          System.out.println(x2);
+          System.out.println(x1 + ", " + x2);
             } else {
            System.out.println("Imaginary values");
             }
